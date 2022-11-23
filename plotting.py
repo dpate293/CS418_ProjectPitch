aa445464e8cc9e07ebccccc3dd96149afa1d66e9
lmora21 <lmora21@uic.edu>
2022-11-23 13:34:37 -0600

    created temps vs pollution complaints scatter plot

--- plotting.py
+++ plotting.py
@@ -18,7 +18,7 @@ df["month"] = df["datetime"].dt.month
 
 df = df.groupby(["month", "COMPLAINT_TYPE"]).COMPLAINT_TYPE.count().reset_index(name="count")
 
-#NOTE: If you want to see the visualization, uncomment the code under ONLY
+#NOTE: If you want to see the complaints visualization, ONLY uncomment the code under
 
 #p = sns.barplot(data=df, x="month", y="count", hue="COMPLAINT_TYPE")
 #p.legend(bbox_to_anchor=(1.1, 1.1),title="Complaint Type", loc= "upper right")
@@ -36,10 +36,24 @@ temps = temps.groupby("month").mean()
 
 temps["month"] = temps.index
 
-#NOTE: If you want to see the visualization, uncomment the code under ONLY
+#NOTE: If you want to see the temps visualization, ONLY uncomment the code under
 
 #t = sns.barplot(data=temps, x="month", y="temp")
 #t.set_ylabel("Average Temperature (F)")
 #t.set_xlabel("Month")
 #t.set_title("Average temperatures for Chicago per month (2021)")
+#plt.show()
+
+
+#NOTE: If you want to see the temps vs complaints visualization, ONLY uncomment the code under
+
+#df.reset_index(drop=True, inplace=True)
+#temps.reset_index(drop=True, inplace=True)
+#df = pd.merge(df, temps, how="inner", on="month")
+#df = df[df["COMPLAINT_TYPE"].isin(["AIR POLLUTION WORK ORDER"])]
+
+#s = sns.regplot(data=df, x="count", y="temp")
+#s.set_ylabel("Temperature (F)")
+#s.set_xlabel("Amount of Pollution Complaints")
+#s.set_title("Temperature vs Amount of Pollution Complaints in Chicago (2021)")
 #plt.show()
